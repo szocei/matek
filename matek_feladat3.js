bf61b6b80c0ffcba5ffcea5d8ff30cbe77af38a5
szocei <szocei.zoltan@gmail.com>
2020-11-26 17:51:12 +0100

    11.26

---
+++ matek_feladat3.js
@@ -0,0 +1,7 @@
+// Írj egy függvényt, ami a paraméterként megadott tízes számrendszerbeli számot átkonvertálja kettes, 
+//nyolcas, és tizenhatos számrendszerbe is! 
+//Ezeket az értékeket pedig egy objectbe adja vissza! A property-k neve legyen: binary, octal, hexa.
+
+function konvert (decnumber) {
+    
+}
