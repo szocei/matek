5f3c006b238dab62737a59b5e58e0567097db1cd
szocei <szocei.zoltan@gmail.com>
2020-11-26 18:36:17 +0100

    3.feladat

--- matek_feladat3.js
+++ matek_feladat3.js
@@ -2,6 +2,14 @@
 //nyolcas, és tizenhatos számrendszerbe is! 
 //Ezeket az értékeket pedig egy objectbe adja vissza! A property-k neve legyen: binary, octal, hexa.
 
-function konvert (decnumber) {
-    
+function konvert (decnum) {
+  console.log( {
+     binary:
+     binarynum = '0b'+ Number(decnum).toString(2),
+     octal: 
+     octalnum= '0o' + Number(decnum).toString(8),
+     hexa: 
+     hexnum = '0x'+ Number(decnum).toString(16)
+        });
 }
+konvert (100);
